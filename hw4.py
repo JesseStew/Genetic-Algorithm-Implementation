a02e1f4d8a191219f81749b4968293312554e7ec
Stewart <jds013@live.missouristate.edu>
2018-05-02 12:03:01 -0500

    Input file, Initialize Population, and Calculate Costs initializePopulation: need to create multiple chromosome variations for each population, or board calcCost: is not calculating correctly.

--- hw4.py
+++ hw4.py
@@ -24,14 +24,65 @@ def main():
 
 """
 #---------------------------------Imports--------------------------------------
-
+import random
 #------------------------------------------------------------------------------
 
 #---------------------------------Variables------------------------------------
 inputFile = 'input1.txt'
+crossoverRate = 0.75
+mutationRate = 0.01
+clear = 100*'\n'
 #------------------------------------------------------------------------------
 
 #---------------------------------Classes/Functions----------------------------
+f = open(inputFile, "r")
+totalInput = [] #input from one input file
+for line in f:
+    lyst = line.split() # tokenize input line, it also removes EOL marker
+    lyst = list(map(int, lyst))
+    totalInput.append(lyst)
+    
+'''
+Step 0.  Algorithm Initialization.  Assume data are encoded in bit strings (1’s and 0’s).
+Specify a crossover probability/rate pc and a mutation probability/rate pm.  
+Usually pc is chosen to be fairly high and pm is chosen to be very low.
+'''
+def initializePopulation(totalInput):
+    #random.randint(0,1)
+    totalEncodedBoardPaths = []
+    for board in totalInput:
+        encodedBoardPath = []
+        for num in range(0, len(board)):
+            if ((num > 0) and (encodedBoardPath[num-1] == 0)) or (num+1 == len(board)) or (num == 0):
+                #print("num + 1 = ", num+1, "  len(board) = ", len(board))
+                encodedBoardPath.append(1)
+            else:
+                encodedBoardPath.append(random.randint(0,1))
+        totalEncodedBoardPaths.append(encodedBoardPath)
+    return totalEncodedBoardPaths
+
+'''
+Returns: a list with the cost of the initialized populations game in the 
+         game's associated index.
+'''
+def calcCost(totalInput):
+    totalEncodedBoardPaths = initializePopulation(totalInput)
+    totalCosts = []
+    for itr in range(0, len(totalInput)):
+        costs = []
+        for num in range(0, len(totalInput[itr])):
+            if totalEncodedBoardPaths[itr][num] == 1:
+                costs.append(totalInput[itr][num])
+        print(costs)
+        totalCosts.append(sum(costs))
+    #is not returning correct costs
+    return totalCosts
+
+'''
+Step 2.  The fitness function f(x) for each chromosome in the population is calculated.
+'''
+#def fitnessFunction():
+
 """
 Description: ...
 Input:       ...
@@ -44,7 +95,6 @@ def getDpSolutions(inputFile):
         lines[1::5] = ['minimum ' + num for num in lines[1::5]]
     solutions = [lines[x*5:x*5+5:] for x in range(int(len(lines)/5))]
     return solutions
-
 
 """
 Description: Takes a list containing the dynamic programming solution for a
@@ -65,7 +115,15 @@ def printDpSolution(dpSolution):
 #---------------------------------Program Main---------------------------------
 def main():
     dpSol = getDpSolutions(inputFile)
-    print(dpSol)
+    encodedPop = initializePopulation(totalInput)
+    costs = calcCost(totalInput)
+    '''print(totalInput)
+    print(encodedPop)
+    print("costs: ", costs)'''
+    for num in range(0, len(totalInput)):
+        print("totalInput[", num, "] = ", totalInput[num])
+        print("encodedPop[", num, "] = ", encodedPop[num])
+        print("costs[", num, "] = ", costs[num])
     printDpSolution(dpSol[0])
     printDpSolution(dpSol[1])
 main()		
